e1bc98acfb274d1c1ee2581f88c765a3750f7288
fenpho <fenpho@163.com>
2019-01-02 20:36:49 +0800

    finished histogram

--- day34-36/bar.js
+++ day34-36/bar.js
@@ -1,26 +1,33 @@
-function drawHistogram({max}) {
+const data = {
+  product: '手机',
+  region: '华东',
+  sale: [120, 100, 140, 160, 180, 185, 190, 210, 230, 245, 255, 270]
+};
+
+function drawHistogram({ maxHeight }) {
   // 定义好柱状图绘制区域的高度，宽度，轴的高度，宽度
   let height = 350;
   let width = 550;
   let vertical = 320;
   let horizontal = 520;
   // 定义好每一个柱子的宽度及柱子的间隔宽度
-  let pillar = 30;
-  let space = 12;
+  let pillarWidth = 28; // 实际宽度30px，2px的边框
+  let pillarSpace = 10; // 实际宽度12px，2px的边框
   // 定义好柱子颜色，轴的颜色
-  let pillarColor = 'blue';
+  let pillarColor = '#01AAED';
   let axesColor = 'black';
   // 拿到柱状图中的最大值Max
-  let maxPillar = max;
+  let maxPillar = maxHeight;
   // 根据Max和你用来绘制柱状图图像区域的高度，进行一个数据和像素的折算比例
   // 用最大的高度的值撑满纵轴，后续值乘以该比例即可的相应比例高度
-  let scale = vertical * vertical / max;
+  let scale = (vertical - 60) / maxPillar;
   // 绘制横轴及纵轴
   let svgArea = `<svg
-  width="550"
-  height="350"
+  width=${width}
+  height=${height}
   version="1.1"
   xmlns="http://www.w3.org/2000/svg"
+  id="histogram"
 >
 <!-- 竖轴 -->
  <line
@@ -28,7 +35,7 @@ function drawHistogram({max}) {
     x2="20"
     y1="20"
     y2="320"
-    stroke="green"
+    stroke=${axesColor}
     fill="transparent"
     stroke-width="1"
   />
@@ -38,7 +45,7 @@ function drawHistogram({max}) {
   x2="20"
   y1="30"
   y2="20"
-  stroke="green"
+  stroke=${axesColor}
   fill="transparent"
   stroke-width="1"
   />
@@ -57,7 +64,7 @@ function drawHistogram({max}) {
     x2="520"
     y1="320"
     y2="320"
-    stroke="green"
+    stroke=${axesColor}
     fill="transparent"
     stroke-width="1"
   />
@@ -67,7 +74,7 @@ function drawHistogram({max}) {
   x2="520"
   y1="310"
   y2="320"
-  stroke="green"
+  stroke=${axesColor}
   fill="transparent"
   stroke-width="1"
   />
@@ -76,17 +83,24 @@ function drawHistogram({max}) {
      x2="520"
      y1="330"
      y2="320"
-     stroke="green"
+     stroke=${axesColor}
      fill="transparent"
      stroke-width="1"
    />
 </svg>`;
+
   // 遍历数据
-  {
-    //     计算将要绘制柱子的高度和位置
-    //     绘制每一个柱子
-  }
-  document.getElementById('histogram').innerHTML = svgArea;
+  const pillar = data.sale.map((v, index) => {
+    // 计算将要绘制柱子的高度和位置
+    let pillarHeight = v * scale;
+    console.log(pillarHeight, v);
+    let leftPosition = 20 + pillarSpace * (index + 1) + index * pillarWidth;
+    // 绘制每一个柱子
+    return `<rect x=${leftPosition} y=${320 - pillarHeight} width=${pillarWidth} height=${pillarHeight} stroke="#1E9FFF" fill=${pillarColor} stroke-width="1" />`;
+  });
+
+  document.getElementById('histogramWrap').innerHTML = svgArea;
+  document.getElementById('histogram').innerHTML += pillar;
 }
 
-drawHistogram();
+drawHistogram({ maxHeight: 270 });
